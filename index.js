eef9d8371d1ee4777310405fd8b2edc8c78664e0
Perc4al <lopez.078934@globalcity.sti.edu.ph>
2024-06-24 10:28:52 +0800

    MoveFilesAround

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static files
-app.use(express.static('public'));
+app.use(express.static(__dirname));
 
 // Route to get data from the CSV file
 app.get('/data', (req, res) => {
